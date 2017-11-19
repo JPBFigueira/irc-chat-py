7d1a6cc466e622739e1d30c3ce1654748f93375f
Cpoiares <carlos.poiares2@gmail.com>
2017-11-19 02:57:10 +0000

    client.py

--- server.py
+++ server.py
@@ -1,71 +1,84 @@
 import socket
-import select
 import sys
+from sys import argv                                #IMPORT LIBRARIES
+from threading import Thread
 import Queue
-
 HOST = 'localhost'
 PORT = 9000
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('ServerSocket created\n')
-serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-inputs = [serverSocket]
-outputs = []
-messages= {}
-try:
-    serverSocket.bind((HOST,PORT))
-except socket.error as msg:
-    print('failed binding\n')
-    sys.exit()
-print('binded\n')
+BUFFER = BUFFER
 
-#Start listening
+def __main__:
 
-serverSocket.listen(10)
-print('ready to listen\n')
+    activeUsers = []
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    activeUsers.append(serverSocket)
+    print('ServerSocket created\n')
+    try:
+        serverSocket.bind((HOST,PORT))
+    except socket.error as msg:
+        print('failed binding\n')
+        sys.exit()
+    print('binded\n')
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    inputs = [serverSocket]
+    outputs = []
+    messages = {}
+    serverSocket.listen(10)
+    print('Listening\n')
 
-inputs.append(serverSocket)
+    while(True):
+        conn, addr = s.accept()
+        print('Connected with ' + addr[0] + ' -> ' + str(addr[1]), end = ' ')
+        thread = Thread(target=cthread, args=(conn, addr))   #Start a thread & connect
+        thread.start()
+#envia mensagens
 
-while(1):
-    readable, writeable, exceptional = select.select(inputs,outputs,inputs)
-    for sock in readable:#new connection received
-        if sock == serverSocket:
-            connection, client_addr = sock.accept()
-            inputs.append(connection)
-            messages[connection] = Queue.Queue()
-        else: #not a new connection
-            string = sock.recv(1024)
-            if string: #something in the socket
-                messages[sock].put(string)
-                if sock not in outputs:
-                    outputs.append(sock)
-            else:#no data, socket is broken
-                if sock in outputs:
-                    outputs.remove(sock)
-                inputs.remove(sock)
-                sock.close()
-                del messages[sock]
+def broadcast_data(sock, message):
+    for socket in activeUsers:
+        if socket !=server_coket and socket != sock:
+            try:
+                socket.send(message)
+            except:
+                print('not only tho\n')
+                socket.close()
+                activeUsers.remove(socket)
+#criar ficheiro de cont
+def cthread(conn):
+    conn.send("hey bro") #TODO encode info?
+    while True:
+        user = conn.recv(BUFFER)
+        user.append(activeUsers)
+        createAcc(user)
+        if(already_logged):
+            print('already logged\n')
+            conn.close()
 
-    for sock in writeable:
-        try:
-            nxt_msg = messages[sock].get_nowait()
-        except Queue.Empty:
-            outputs.remove(sock)
+        conn.send('gj nice user name '+ user + '\n')
+        dest = conn.recv(BUFFER)
+        while True:
+            message = conn.recv(BUFFER)
+            if dest in activeUsers:
+                send_msg(user, dest, msg)
+            else:
+                conn.send('not online\n')
 
-        else:
-            nxt_msg = nxt_msg + 'Fully Connected\n'
-            sock.send(nxt_msg)
+def createAcc(user):
+    file = open(user, 'w+')
+    f.close()
 
+def already_logged(user):
+    for user in activeUsers:
+        if user[0] == user:
+            return True
+    return False
 
+def broadcast(user):
+    message = 'Message from ' + user ': '
+    for user_tuple in activeUsers:
+        user_tuple[1].sendall(message)
 
-#metodo broadcast para todos os clientes
-#def broadcast(server_socket, sock, message):
-#    for socket in socket_list:
-#        if socket!= serverSocket and socket != sock:
-#            try:
-#                socket.send(message)
-#            except:
-#                #broken conn
-#                socket.close()
-#                #broken socketfd
-#                if socket in socket_list:
-#                    socket_list.remove(socket)
+def send_msg(username, dest, msg):
+    f = dest + 'txt'
+    file = open(f, 'w+')
+    f.writelines(user +'\n' +msg + '\n' + '-' + '\n')
+    f.close()
